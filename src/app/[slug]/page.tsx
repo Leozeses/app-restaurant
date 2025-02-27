1225bdedabcdaf8b5c699982e8e8942f8a8a4bf5
Leonardo <leonardolemos100@gmail.com>
2025-02-27 09:30:49 -0300

    start intro page

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,48 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { db } from "@/lib/prisma";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const RestaurantPages = async ({ params }: RestaurantPageProps) => {
+  const { slug } = await params;
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant?.avatarImageUrl}
+          alt={restaurant.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+      <div className="space-y-2 pt-24 text-center">
+        <h3 className="text-2xl font-semibold">Seja Bem-Vindo!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos aqui para
+          oferecer praticidade e sabor em cada detalhe!
+        </p>
+      </div>
+      <div className="grid grid-cols-2 pt-14"></div>
+      <Card>
+        <CardContent className="flex flex-col items-center gap-8 py-8"></CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default RestaurantPages;
+
+// server components - renderizados no servidor
+// podem ser async
+// podem chamar recursos do back-end (banco de dados)
+// não pode usar hooks
+// não pode ter interatividade
