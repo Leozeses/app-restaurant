9b888d9c8ffd4d0bf5ea8a3eebcd366383de70e4
Leonardo <leonardolemos100@gmail.com>
2025-03-24 18:04:17 -0300

    feat: ensure cart product quantity can be decrease

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -12,13 +12,15 @@ export interface ICartContext {
   products: CartProduct[];
   toggleCart: () => void;
   addProduct: (product: CartProduct) => void
+  decreaseProductQuantity: (productId: string) => void
 }
 
 export const CartContext = createContext<ICartContext>({
   isOpen: false,
   products: [],
   toggleCart: () => {},
-  addProduct: () => {}
+  addProduct: () => {},
+  decreaseProductQuantity: () => {}
 });
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -49,6 +51,23 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         return prevProduct
       })
     })
+  };
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts(prevProducts => {
+      return prevProducts.map(prevProduct => {
+        if (prevProduct.id != productId) {
+          return prevProduct;
+        }
+        if (prevProduct.quantity == 1) {
+          return prevProduct;
+        }
+        return {
+            ...prevProduct,
+            quantity: prevProduct.quantity - 1
+          }
+      })
+    }
+    )
   }
   return (
     <CartContext.Provider
@@ -56,7 +75,8 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         isOpen,
         products,
         toggleCart,
-        addProduct
+        addProduct,
+        decreaseProductQuantity,
       }}
     >
       {children}
